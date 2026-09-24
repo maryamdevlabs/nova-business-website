07d212beb7b26af1c8d7f27c85d93920e41c09d1
Maryam <maryam.devlabs@gmail.com>
2026-09-24 23:13:41 +0500

    Fix component import casing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/hero"
 import Services from "./components/services"
 import About from "./components/About"
 import Testimonials from "./components/testimonials"
-import CTA from "./components/CTA"
+import CTA from "./components/cta"
 import Contact from "./components/contact"
 import Footer from "./components/footer"
 function App () {
