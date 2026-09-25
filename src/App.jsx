6d036622d008adfc0e11f7ea7e463a156459858e
Maryam <maryam.devlabs@gmail.com>
2026-09-25 15:53:59 +0500

    Fix CTA import case

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/hero"
 import Services from "./components/services"
 import About from "./components/about"
 import Testimonials from "./components/testimonials"
-import CTA from "./components/cta"
+import CTA from "./components/CTA"
 import Contact from "./components/contact"
 import Footer from "./components/footer"
 function App () {
